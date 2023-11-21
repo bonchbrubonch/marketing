31847263efef932e832a63928b2184f394b498da
Artem <nikolloteslla@gmail.com>
2023-11-21 12:29:32 +0200

    add section

--- app/js/app.js
+++ app/js/app.js
@@ -18,8 +18,94 @@ $(document).ready(function () {
   });
 
 
-
 });
+
+
+var number1 = document.querySelector('.num1'),
+  number1Top = number1.getBoundingClientRect().top,
+  start = +number1.innerHTML,
+  end = +number1.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+  if (window.pageYOffset > number1Top - window.innerHeight / 1.2) {
+    this.removeEventListener('scroll', onScroll);
+    var interval = setInterval(function () {
+      number1.innerHTML = ++start;
+      if (start == end) {
+        clearInterval(interval);
+      }
+    }, 5);
+  }
+});
+
+var number2 = document.querySelector('.num2'),
+  number2Top = number2.getBoundingClientRect().top,
+  start2 = +number2.innerHTML,
+  end2 = +number2.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+  if (window.pageYOffset > number2Top - window.innerHeight / 1.2) {
+    this.removeEventListener('scroll', onScroll);
+    var interval = setInterval(function () {
+      number2.innerHTML = ++start2;
+      if (start2 == end2) {
+        clearInterval(interval);
+      }
+    }, 5);
+  }
+});
+
+var number3 = document.querySelector('.num3'),
+  number3Top = number3.getBoundingClientRect().top,
+  start3 = +number3.innerHTML,
+  end3 = +number3.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+  if (window.pageYOffset > number3Top - window.innerHeight / 1.2) {
+    this.removeEventListener('scroll', onScroll);
+    var interval = setInterval(function () {
+      number3.innerHTML = ++start3;
+      if (start3 == end3) {
+        clearInterval(interval);
+      }
+    }, 5);
+  }
+});
+
+var number4 = document.querySelector('.num4'),
+  number4Top = number4.getBoundingClientRect().top,
+  start4 = +number4.innerHTML,
+  end4 = +number4.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+  if (window.pageYOffset > number4Top - window.innerHeight / 1.2) {
+    this.removeEventListener('scroll', onScroll);
+    var interval = setInterval(function () {
+      number4.innerHTML = ++start4;
+      if (start4 == end4) {
+        clearInterval(interval);
+      }
+    }, 5);
+  }
+});
+
+var number5 = document.querySelector('.num5'),
+  number5Top = number5.getBoundingClientRect().top,
+  start5 = +number5.innerHTML,
+  end5 = +number5.dataset.max;
+
+window.addEventListener('scroll', function onScroll() {
+  if (window.pageYOffset > number5Top - window.innerHeight / 1.2) {
+    this.removeEventListener('scroll', onScroll);
+    var interval = setInterval(function () {
+      number5.innerHTML = ++start5;
+      if (start5 == end5) {
+        clearInterval(interval);
+      }
+    }, 3);
+  }
+});
+
 
 
 
